aed251e025f02c97b6b06de3cb134911ae0e559a
Daniel <dwimmer1@htl-steyr.ac.at>
2022-09-20 09:38:04 +0200

    Unessescary File delete FXML weiterleitung von Menue in PlayfieldFXML mittels Button

---
+++ src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/MenueController.java
@@ -0,0 +1,14 @@
+package com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer;
+
+import java.io.IOException;
+
+public class MenueController {
+
+
+
+
+    public void switchToPlayfield() throws IOException {
+        HelloApplication.setRoot("playField-view");
+
+    }
+}

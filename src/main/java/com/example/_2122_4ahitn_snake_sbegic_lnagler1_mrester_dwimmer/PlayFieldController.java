87c01df97c5037e8abad53bafcb45a184f22d7ef
lukas <lnagler1@htl-steyr.ac.at>
2022-09-20 10:37:34 +0200

    playField(first push)

---
+++ src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/PlayFieldController.java
@@ -0,0 +1,22 @@
+package com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PlayFieldController {
+
+    GridPane gridPane = new GridPane();
+    HBox hbox = new HBox();
+    VBox vBox = new VBox();
+
+    public void initialize(){
+        Playfield playfield = new Playfield();
+        gridPane = playfield.createGridPane(50, 50);
+        System.out.println(gridPane.toString());
+        vBox.getChildren().add(gridPane);
+        hbox.getChildren().add(vBox);
+        System.out.println("Bin drin");
+    }
+}

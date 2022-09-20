da417b13e550c821da52ae4503ed6e56a5c42eb5
dwimmer1 <dwimmer1@htl-steyr.ac.at>
2022-09-20 08:40:16 +0200

    Add files via upload

---
+++ src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/HelloController.java
@@ -0,0 +1,14 @@
+package com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer to javafx.fxml;
+    exports com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer;
+}

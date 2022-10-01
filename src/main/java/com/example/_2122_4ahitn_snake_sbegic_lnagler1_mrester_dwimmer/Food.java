6d16d339199d4b194bd9de9e0d5917d45141821e
lnagler1 <lnagler1@htl-steyr.ac.at>
2022-10-01 16:57:18 +0200

    Food class

--- src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/Food.java
+++ src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/Food.java
@@ -4,7 +4,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class Food {
-    private final String foodString = "assets/icon/ic_apple.png";
+    private final String[] foodString = {"https://i.pinimg.com/originals/58/25/d5/5825d50c11a14cce6e80239de736d3c7.png", "https://www.mcdonalds.at/wp-content/uploads/2021/09/web-neu-1500x1500-family-happy-meal-1.png", "https://www.mcdonalds.at/wp-content/uploads/2021/09/web-neu-1500x1500-easy-morning-mcmuffin-bacon-and-egg-1-480x480.png"};
     private static int foodX;
     private static int foodY;
     private Image foodImage;
@@ -12,7 +12,7 @@ public class Food {
     public void randomFood(int rows, int cols){
         foodX = (int) (Math.random() * rows);
         foodY = (int) (Math.random() * cols);
-        foodImage = new Image("https://i.pinimg.com/originals/58/25/d5/5825d50c11a14cce6e80239de736d3c7.png", 50, 50, false, false);
+        foodImage = new Image(foodString[(int) (Math.random() * foodString.length)], 100, 100, false, false);
     }
 
 

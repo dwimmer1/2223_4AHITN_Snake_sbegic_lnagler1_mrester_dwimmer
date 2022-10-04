5ea33581b31b80db46cda9c0d0a391800f5cdac4
mrester <95427647+mrester@users.noreply.github.com>
2022-10-04 12:56:02 +0200

    Music abspielbar kann zwischen 4 Songs wechseln mittels Schwierigkeitsänderung GIFs für Schwierigkeiten

--- src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/MenueController.java
+++ src/main/java/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/MenueController.java
@@ -1,29 +1,98 @@
 package com.example._2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MenueController {
 
-
+    @FXML
     public Button btExit;
+
+    @FXML
     public ChoiceBox cbChooseSpeed;
+
+    @FXML
     public Button btMusicOnOff;
+
+    @FXML
     public Slider slVolume;
+
+    @FXML
     public Button playBTN;
+
+    @FXML
+    public ImageView imageView;
+
+    Boolean music_status = true;   //gets Value from Menue-Input (Default Value = true)
+
+    private Media media;
+
+    private MediaPlayer mp;
+
 
     public void initialize() throws IOException {
         cbChooseSpeed.setItems(FXCollections.observableArrayList(
                 "Leicht", "Normal", "Schwer", "0/10Yasuo"));
-        }
+
+        cbChooseSpeed.setValue("Leicht");
+        String[] gifs = {
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/GIFs/Easy-Mode-GIF.gif",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/GIFs/Normal-Mode-GIF.gif",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/GIFs/Difficult-Mode-GIF.gif",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/GIFs/yasuo_face_the_wind.gif"
+        };
+        String[] songs = {
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/music/Easy-Mode-Music.mp3",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/music/Normal-Mode-Music.mp3",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/music/Difficult-Mode-Music.mp3",
+                "src/main/resources/com/example/_2122_4ahitn_snake_sbegic_lnagler1_mrester_dwimmer/music/0_10Yasuo-Mode-Music.mp3"
+        };
+
+        imageView.setImage(new Image(new File(gifs[0]).toURI().toString()));
+        media = new Media(new File(songs[0]).toURI().toString());
+        createMediaPlayer();
+        setVolume4Song();
+
+        cbChooseSpeed.getSelectionModel().selectedIndexProperty().addListener(
+                (ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+                    if (Integer.parseInt(String.valueOf(new_val)) == 0) {
+                        setSongAndGif(gifs, songs, Integer.parseInt(String.valueOf(new_val)));
+                    } else if (Integer.parseInt(String.valueOf(new_val)) == 1) {
+                        setSongAndGif(gifs, songs, Integer.parseInt(String.valueOf(new_val)));
+                    } else if (Integer.parseInt(String.valueOf(new_val)) == 2) {
+                        setSongAndGif(gifs, songs, Integer.parseInt(String.valueOf(new_val)));
+                    } else if (Integer.parseInt(String.valueOf(new_val)) == 3) {
+                        setSongAndGif(gifs, songs, Integer.parseInt(String.valueOf(new_val)));
+                    }
+                });
+    }
+
+    private void setSongAndGif(String[] gifs, String[] songs, int new_val) {
+        mp.stop();
+        imageView.setImage(null);
+        imageView.setImage(new Image(new File(gifs[Integer.parseInt(String.valueOf(new_val))]).toURI().toString()));
+        media = new Media(new File(songs[Integer.parseInt(String.valueOf(new_val))]).toURI().toString());
+        createMediaPlayer();
+    }
+
+    private void setVolume4Song() {
+        slVolume.setValue(mp.getVolume() * 100);
+        slVolume.valueProperty().addListener(observable -> mp.setVolume(slVolume.getValue() / 100));
+        System.out.println(mp.getStatus());
+    }
 
 
     public void switchToPlayfield() throws IOException {
@@ -37,6 +106,19 @@ public class MenueController {
     }
 
     public void musicOnOff(ActionEvent actionEvent) {
+        music_status = !music_status;
+        if (music_status) {
+            createMediaPlayer();
+        } else {
+            mp.stop();
+        }
+    }
 
+    public void createMediaPlayer() {
+        mp = new MediaPlayer(media);
+        mp.setAutoPlay(music_status);
+        mp.setVolume(0.5);
+        slVolume.setValue(mp.getVolume() * 100);
+        mp.setCycleCount(MediaPlayer.INDEFINITE);
     }
 }
